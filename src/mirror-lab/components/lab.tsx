5f0720455312d98234ffe232fb9e1bd636d33704
Nicole W. <76132106+nicolecomputer@users.noreply.github.com>
2025-05-03 21:10:03 +0000

    Refactor lab to use world size
    This lays the foundation for simulation sizes that aren't just 500x500

--- src/mirror-lab/components/lab.tsx
+++ src/mirror-lab/components/lab.tsx
@@ -5,15 +5,23 @@ It is meant to be wrapped in a <StateProvider /> or it will not work correctly
 */
 "use client"
 
+import { useState } from "@/lib/StateContext"
 import Observables from "../optics-components/Observables"
 import Observer from "../optics-components/Observer"
 import Reflections from "../optics-components/Reflections"
 import VerticalMirrors from "../optics-components/VerticalMirrors"
 import "./style.css"
+import { State } from "@/core/reducer/types"
 
 export default function MirrorLab() {
+    const state: State = useState()
+
     return (
-        <div className="mirror-lab">
+        <div className="mirror-lab"
+            style={{
+                width: state.world.width,
+                height: state.world.height
+            }}>
             <Observer />
             <VerticalMirrors />
             <Observables />
